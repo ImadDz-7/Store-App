535f02edaaaad3a694e07d4b136281cc3f7c59b3
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-09-01 23:35:25 +0000

    Refactor all services by using api class to retrieve and decode data and checking exceptions

--- lib/services/get_category_service.dart
+++ lib/services/get_category_service.dart
@@ -1,27 +1,21 @@
 import 'dart:convert';
 
+import 'package:store_app/helper/api.dart';
 import 'package:store_app/models/product_model.dart';
 import 'package:http/http.dart' as http;
 
 class GetCategoryService {
   Future<List<ProductModel>> getProductsByCategory(
       {required String categoryName}) async {
-    http.Response response = await http.get(
-        Uri.parse('https://fakestoreapi.com/products/category/$categoryName'));
+    List<dynamic> data = await Api()
+        .get(url: 'https://fakestoreapi.com/products/category/$categoryName');
 
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
+    List<ProductModel> productsList = [];
 
-      List<ProductModel> productsList = [];
-
-      for (int i = 0; i < data.length; i++) {
-        productsList.add(ProductModel.fromJson(data[i]));
-      }
-
-      return productsList;
-    } else {
-      throw Exception(
-          'There is a problem with statusCode ${response.statusCode}');
+    for (int i = 0; i < data.length; i++) {
+      productsList.add(ProductModel.fromJson(data[i]));
     }
+
+    return productsList;
   }
 }

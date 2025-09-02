3cd751dd74df4e444ddb9ce548a94f94a6e34f2d
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-09-02 21:33:11 +0000

    Create poste methode and make poste request

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -12,5 +12,21 @@ class Api {
       throw Exception(
           'There is a problem with statusCode ${response.statusCode}');
     }
+  }
+
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response =
+        await http.post(Uri.parse(url), body: body, headers: headers);
+
+    Map<String, dynamic> data = jsonDecode(response.body);
+
+    return data;
   }
 }

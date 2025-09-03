386160611127ed4bb2900e0ac08ef7f9bb7f2906
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-09-03 11:01:13 +0000

    Create put methode to update product

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -31,7 +31,33 @@ class Api {
       return data;
     } else {
       throw Exception(
-          'There is a problem with statusCode ${response.statusCode}');
+          'There is a problem with statusCode ${response.statusCode} with body ${jsonDecode(response.body)}');
+    }
+  }
+
+  Future<dynamic> put({
+    required String url,
+    @required dynamic body,
+    @required String? token,
+    required int id,
+  }) async {
+    Map<String, String> headers = {};
+    headers.addAll({'Content-Type': 'application/x-www-form-urlencoded'});
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.put(
+      Uri.parse('https://fakestoreapi.com/products/$id'),
+      body: body,
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          'There is a problem with statusCode ${response.statusCode} with body ${response.body}');
     }
   }
 }

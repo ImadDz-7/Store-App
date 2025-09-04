99df8346e8c903a4519248c4d8f153d6d5d831c3
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-09-04 10:36:03 +0000

    Extract the design of the product card in a CustomCard widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:store_app/widgets/custom_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -25,68 +26,8 @@ class HomePage extends StatelessWidget {
               )),
         ],
       ),
-      body: Center(
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Container(
-              width: 130,
-              height: 100,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 40,
-                    color: Colors.grey.withOpacity(.1),
-                    spreadRadius: 0,
-                    offset: Offset(10, 10),
-                  ),
-                ],
-              ),
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Handbag LV',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 15,
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            r'$225',
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              FontAwesomeIcons.solidHeart,
-                              color: Colors.red,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              left: 40,
-              bottom: 70,
-              child: Image.network(
-                'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
-                height: 90,
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: CustomCard(),
     );
   }
 }
+

a11ba7e3c69366f95c9eb765b737da9118f483b6
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-09-05 14:05:32 +0000

    Create Update product page widget

---
+++ lib/pages/update_product_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UpdateProductPage extends StatelessWidget {
+  const UpdateProductPage({super.key});
+
+  static String id = 'UpdateProductPage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

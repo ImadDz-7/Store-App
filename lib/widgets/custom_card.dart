cac869df43da39f8ec48e32941dab2f27085c9e4
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-09-05 14:29:09 +0000

    Navigate to Update product page

--- lib/widgets/custom_card.dart
+++ lib/widgets/custom_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:store_app/models/product_model.dart';
+import 'package:store_app/pages/update_product_page.dart';
 
 class CustomCard extends StatelessWidget {
   CustomCard({
@@ -12,64 +13,69 @@ class CustomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.none,
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 40,
-                color: Colors.grey.withOpacity(.1),
-                spreadRadius: 0,
-                offset: Offset(10, 10),
-              ),
-            ],
-          ),
-          child: Card(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    product.title.toString().substring(0, 6),
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 15,
+    return GestureDetector(
+      onTap: (){
+        Navigator.pushNamed(context, UpdateProductPage.id);
+      },
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 40,
+                  color: Colors.grey.withOpacity(.1),
+                  spreadRadius: 0,
+                  offset: const Offset(10, 10),
+                ),
+              ],
+            ),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.title.toString().substring(0, 6),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                      '\$${product.price.toString()}',
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          FontAwesomeIcons.solidHeart,
-                          color: Colors.red,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                        '\$${product.price.toString()}',
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            FontAwesomeIcons.solidHeart,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        Positioned(
-          left: 60,
-          bottom: 65,
-          child: Image.network(
-            product.image,
-            height: 90,
-            width: 90,
+          Positioned(
+            left: 60,
+            bottom: 65,
+            child: Image.network(
+              product.image,
+              height: 90,
+              width: 90,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
